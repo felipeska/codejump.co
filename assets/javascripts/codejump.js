a80d3e3f23351a7ba78fc0d7bf0fbe10af6c2a14
Felipe Calderon Barragan <felipeskabarragan@gmail.com>
2014-04-02 02:18:43 -0500

    general update

---
+++ assets/javascripts/codejump.js
@@ -0,0 +1,21 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-custom a").css("color", "#474747");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-custom a").css("color", "#FFF");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
